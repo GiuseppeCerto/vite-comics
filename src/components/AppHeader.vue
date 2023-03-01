8097dad18c49b888aa428ae92b09a842f0b8a7eb
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-03-01 16:31:37 +0100

    EDIT header items

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,21 @@
 <template>
-    <header class="main-header">
+    <header class="container">
+        <div class="menu">
+            <img class="logo_header" src="/images/dc-logo.png" alt="">
 
+            <ul class="menu_header">
+                <li>characters</li>
+                <li>comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
+        </div>
     </header>
   </template>
   
@@ -11,6 +26,19 @@
   </script>
   
   <style lang="scss" scoped>
+    .logo_header{
+        width: 82px;
+    }
 
+    .menu{
+        display: flex;
+        gap: 470px;
+    }
+
+    .menu_header{
+        display: flex;
+        align-items: center;
+        gap: 30px;
+    }
   
   </style>
