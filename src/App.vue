f187efb523b1931a222bc7febcc6cf10d2c84e6e
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-03-01 16:09:42 +0100

    ADD sass

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
-<script setup>
-
+<script>
+import AppFooter from './components/AppFooter.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+export default {
+  components: {
+    AppFooter,
+    AppHeader,
+    AppMain,
+  }
+}
 </script>
 
 <template>
-
+  <div>
+    <AppHeader />
+    <AppMain />
+    <AppFooter />
+  </div>
 </template>
 
-<style scoped>
-
+<style lang="scss">
+@use './style/general.scss';
 </style>
